1cdd86499d6d7e73d13e3b7eb9e303753a91931e
Michael Kafka <daylife.mike@gmail.com>
2016-03-20 18:25:24 -0400

    -- garbage

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject photomosaic "0.3.0-SNAPSHOT"
+  :description "Gallery layouts - available as a JS plugin and for WordPress."
+  :url "https://github.com/daylifemike/photomosaic"
+  ; :license {:name "Eclipse Public License"
+  ;           :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.match "0.2.2"]
+                ]
+  ; :main ^:skip-aot pm3.core
+  ; :target-path "target/%s"
+  ; :profiles {:uberjar {:aot :all}}
+)
---
+++ src/pm3/core.clj
@@ -0,0 +1,7 @@
+(ns pm3.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
